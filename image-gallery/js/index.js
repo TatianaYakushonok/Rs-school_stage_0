586544f9edb23170f30933cc91b23d3715507a1b
TatianaYakushonok <yakush2@yandex.ru>
2022-02-11 19:09:17 +0700

    feat: image output via search

--- image-gallery/js/index.js
+++ image-gallery/js/index.js
@@ -1,5 +1,5 @@
 
-const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+let url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 
 async function getData() {
     const res = await fetch(url);
@@ -8,8 +8,6 @@ async function getData() {
     showData(data);
   }
   getData();
-
-//console.log("data.urls.regular");
 
 const galleryContainer = document.querySelector('.gallery-container');
 
@@ -21,8 +19,18 @@ function showData(data) {
         img.width = 420;
         img.height = 270;
         img.style.padding = '10px';
+        img.style.borderRadius = '20px';
         img.src = `${imgSrc}`;
         img.alt = `image`;
         galleryContainer.append(img);
     })
 }
+
+const input = document.querySelector('.header-search');
+const searchBtn = document.querySelector('.search-btn');
+
+input.addEventListener('change', function(event) {
+    url = `https://api.unsplash.com/search/photos?query=${event.target.value}&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
+    galleryContainer.innerHTML = '';
+    getData();
+}) 
