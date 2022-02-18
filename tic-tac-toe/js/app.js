01f735f5a372939ee79b4d11e0b28e68548d6cc4
TatianaYakushonok <yakush2@yandex.ru>
2022-02-18 19:06:40 +0700

    feat: add player winner

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -6,12 +6,19 @@ let featurePlayer1 = 'X';
 let featurePlayer2 = '0';
 //let namePayer = '';
 
+const boxes = document.querySelectorAll('.box');
+
 const nameContainer = document.querySelector('.name-container');
 const nameOverlay = document.querySelector('.name-overlay');
 const inputName1 = document.querySelector('.text-name1');
 const inputName2 = document.querySelector('.text-name2');
 const namePlayer1 = document.querySelector('.name-player1');
 const namePlayer2 = document.querySelector('.name-player2');
+
+const windowContainer = document.querySelector('.window-container');
+const contentWindow = document.querySelector('.content');
+const windowOverlay = document.querySelector('.window-overlay');
+const BtnClose = document.querySelector('.btn-close');
 
 window.onload = function() {
     nameContainer.style.display = 'block';
@@ -39,6 +46,20 @@ inputName2.addEventListener('keyup', event => {
     }
 })
 
+function showWinner(winner) {
+    contentWindow.innerHTML = `Player ${winner} has won!`;
+    windowContainer.style.display = 'block';
+    boxes.forEach((item)  => {
+        item.textContent = '';
+    })
+}
+
+function closeModal() {
+    windowContainer.style.display = 'none'; 
+}
+
+BtnClose.addEventListener('click', closeModal);
+
 function determWinnings() {
     const boxes = document.querySelectorAll('.box');
 
@@ -58,9 +79,7 @@ function determWinnings() {
 		    boxes[arrBox[i][0]].innerHTML == 'X' && boxes[arrBox[i][1]].innerHTML == 'X' && boxes[arrBox[i][2]].innerHTML == 'X'
 		) {
             namePlayer1.innerHTML = `${inputName1.value} : ${featurePlayer1} ${resultPlayer1+=1}`;
-            boxes.forEach((item)  => {
-                item.textContent = '';
-            })
+            showWinner(inputName1.value);
             //result = 'крестики';
             //console.log('Победили крестики');
 			//prepareResult(result);
@@ -69,6 +88,7 @@ function determWinnings() {
 		    boxes[arrBox[i][0]].innerHTML == '0' && boxes[arrBox[i][1]].innerHTML == '0' && boxes[arrBox[i][2]].innerHTML == '0'
 		) {
             namePlayer2.innerHTML = `${inputName2.value} : ${featurePlayer2} ${resultPlayer2+=1}`;
+            showWinner(inputName2.value);
             //result = 'нолики';
             //console.log('Победили нолики');
 			//prepareResult(result);
